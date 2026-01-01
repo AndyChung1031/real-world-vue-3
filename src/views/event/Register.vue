381bd7d4514788aeb3ad08ca7ac372c4baf260a0
Andy Chung <arosa.andy@gmail.com>
2026-01-01 17:25:56 +0700

    Set global store for flash messages

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,16 @@
 <script setup>
 import { useRouter } from 'vue-router'
+import { inject } from 'vue'
+
 const props = defineProps(["event"])
 const router = useRouter()
+const GStore = inject('GStore')
 
 const register = () => {
+        GStore.flashMessage = 'You are successfully registered for ' + props.event.title
+        setTimeout(()=> {
+                GStore.flashMessage = ''
+        }, 3000)
         router.push({
                 name: 'event-details',
                 id: props.event.id,
