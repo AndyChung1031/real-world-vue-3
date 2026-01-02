ee7244298e6932c2cb5e072a32cfe42b7717a08c
Andy Chung <arosa.andy@gmail.com>
2026-01-02 16:50:17 +0700

    Custom types using types.ts

--- src/views/event/Details.vue
+++ src/views/event/Details.vue
@@ -1,5 +1,9 @@
-<script setup>
+<script setup lang="ts">
+import { EventItem } from '@/types' 
+
 const props = defineProps(["event"])
+const event = {} as EventItem // custom type from types.ts.. event object only allows properties defined in types.ts
+
 </script>
 
 <template>
